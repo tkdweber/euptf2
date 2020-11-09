7b0f7ef658a42deedc35284714e3cba631bb4740
TothSzaboBrigitta <60616407+TothSzaboBrigitta@users.noreply.github.com>
2020-11-09 22:43:29 +0100

    Update ptf.transFun.R
    change name of parameters

--- R/ptf.transFun.R
+++ R/ptf.transFun.R
@@ -12,13 +12,13 @@
 ptf.transFun <- function(ptf.L){
  res.trans <- list()
 
- res.trans$ths   <-  function(x) x
- res.trans$thr.t <-  function(x) 10^x-1
- res.trans$alf.t <-  function(x) 10^x
- res.trans$n.t   <-  function(x) 10^x+1
+ res.trans$THS   <-  function(x) x
+ res.trans$THR <-  function(x) 10^x-1
+ res.trans$ALP <-  function(x) 10^x
+ res.trans$N   <-  function(x) 10^x+1
  if(length(ptf.L) == 6){
-         res.trans$ks.t  <-  function(x) 10^x
-         res.trans$tau   <-  function(x) x
+         res.trans$K0  <-  function(x) 10^x
+         res.trans$L   <-  function(x) x
  }
  res.df <- mapply(function(f, x) f(x), res.trans, ptf.L)
 
