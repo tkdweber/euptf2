2322ca2bfc51053563ac4ad1da8faf921b35ecbe
tkdweber <tobias.weber@uni-hohenheim.de>
2020-11-18 15:45:20 +0100

    created documentation files

---
+++ inst/doc/euptf2_v2.R
@@ -0,0 +1,15 @@
+library(rmarkdown)
+
+usethis::use_vignette("vignette_v2")
+
+browseVignettes("rmarkdown")
+
+---
+  title: "R package of updated European hydraulic pedotransfer functions (euptf2)"
+author: "Weber, Tobias, Weynants, Melanie and Szabó, Brigitta"
+output: rmarkdown::html_vignette
+vignette: >
+  %\VignetteIndexEntry{Vignette Title}
+%\VignetteEngine{knitr::rmarkdown}
+%\VignetteEncoding{UTF-8}
+---
---
+++ inst/doc/vignette_v2.R
@@ -0,0 +1,85 @@
+## ---- include = FALSE----------------------------------------------------------------------------------------------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----eval=FALSE----------------------------------------------------------------------------------------------------------------------------------------------------
+#  # install the devtools package if not yet done:
+#  if (!require("devtools")){install.packages("devtools"); library(devtools)}
+#  if (!require("euptf2")){devtools::install_github("tkdweber/euptf2"); library(euptf2)}
+
+## ----setup---------------------------------------------------------------------------------------------------------------------------------------------------------
+# load the package:
+library(euptf2)
+
+## ----eval=FALSE----------------------------------------------------------------------------------------------------------------------------------------------------
+#  data(sample_data)
+
+## ----eval=TRUE-----------------------------------------------------------------------------------------------------------------------------------------------------
+sample_data
+
+## ----eval=FALSE----------------------------------------------------------------------------------------------------------------------------------------------------
+#  # import data from csv file:
+#  mydata <- read.csv("myworksheet.csv")
+#  
+#  # import data from xlsx file with package xlsx:
+#  # install the xlsx package if not yet done:
+#  if (!"xlsx" %in% IP){install.packages("xlsx")}
+#  # load the package:
+#  library("xlsx")
+#  # import data from xlsx file:
+#  mydata <- read.xlsx("myworkbook.xlsx", sheetName="myworksheet")
+#  # see ?read.xlsx for more options
+
+## ----eval=TRUE-----------------------------------------------------------------------------------------------------------------------------------------------------
+# check which_PTF to use to predict THS based on the predictor variables available in sample_data
+which_PTF(predictor= sample_data, target = c("THS"))
+
+# check which_PTF to use to predict multiple soil hydraulic properties
+which_PTF(predictor= sample_data, target = c("THS", "FC", "WP", "KS", "VG", "MVG"))
+
+## ----eval=FALSE----------------------------------------------------------------------------------------------------------------------------------------------------
+#  data(suggested_PTF)
+#  suggested_PTF
+
+## ----eval=TRUE-----------------------------------------------------------------------------------------------------------------------------------------------------
+# predict parameters of the van Genuchten model (VG)
+pred_VG <- euptfFun(ptf = "PTF11", predictor = sample_data, target = "VG", query = "predictions")
+pred_VG
+
+# predict VG with predefined quantiles
+pred_VG_q <- euptfFun(ptf = "PTF11", predictor = sample_data, target = "VG", query = "quantiles", quantiles = c(0.1, 0.5, 0.9))
+pred_VG_q
+
+# please note that output is list, can be formatted to data frame:
+pred_VG_q.df <- as.data.frame(pred_VG_q)
+
+# predict parameters of the Mualem-van Genuchten model (MVG)
+pred_MVG <- euptfFun(ptf = "PTF05", predictor = sample_data, target = "MVG", query = "predictions")
+pred_MVG
+
+## ----eval=TRUE-----------------------------------------------------------------------------------------------------------------------------------------------------
+# predict saturated water content (THS)
+pred_THS <- euptfFun(ptf = "PTF03", predictor = sample_data, target = "THS", query = "predictions")
+pred_THS
+
+# predict THS with predefined quantiles
+pred_THS_q <- euptfFun(ptf = "PTF03", predictor = sample_data, target = "THS", query = "quantiles", quantiles = c(0.05, 0.5, 0.95))
+pred_THS_q
+
+# predict saturated hydraulic conductivity (KS)
+pred_KS <- euptfFun(ptf = "PTF05", predictor = sample_data, target = "KS", query = "predictions")
+pred_KS
+
+## ----eval=FALSE----------------------------------------------------------------------------------------------------------------------------------------------------
+#  # example to save the predicted values to a csv file:
+#  write.csv(pred_THS, file = "pred_THS.csv", row.names = FALSE)
+#  # see ?write.csv for additional options
+#  
+#  # example to save predicted values to xlsx file:
+#  # install the openxlsx package if not yet done:
+#  if (!"openxlsx" %in% IP){install.packages("openxlsx")}
+#  library(openxlsx)
+#  write.xlsx(data.KS.VG, file = "pred_THS.xlsx")
+
