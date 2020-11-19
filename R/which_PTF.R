6c574c613b2d59d8133ecc8936406338e1e11b20
tkdweber <tobias.weber@uni-hohenheim.de>
2020-11-19 11:42:50 +0100

    Corrected the search function, now gives correct result

--- R/which_PTF.R
+++ R/which_PTF.R
@@ -11,7 +11,7 @@
 #'
 #' @details The following columns in the predictor argument can be present, the first 4 are required for the code to work. The names are case sensitive.
 #' USSAND, USSILT, USCLAY, DEPTH_M, OC, BD, CACO3, PH_H2O, CEC
-#' 
+#'
 #'
 #' @references
 #' @author Tobias KD Weber, \email{tobias.weber@uni-hohenheim.de}
@@ -44,8 +44,8 @@ which_PTF <- function(predictor, target = NULL) {
         if(sum(c("USSAND", "USSILT","USCLAY", "DEPTH_M")%in%predictor)!=4){stop("predictor has to have USSAND, USSILT, USCLAY, and DEPTH_M as columnnames")}
 
         ## target
-        if(all(is.character(target))!=TRUE ){stop("argument target has to be a character or default NULL")}
-        if(is.null(target)){target = c("USSAND", "USSILT","USCLAY", "DEPTH_M", "OC", "BD", "CACO3", "PH_H20", "CEC")}
+        if(all(is.character(target))!=TRUE & !is.null(target) ){stop("argument target has to be a character or default NULL")}
+        if(is.null(target)){target = c("THS",  "FC_2", "FC", "WP", "AWC_2", "AWC", "KS", "VG", "MVG")}
 
         # LOAD ----
         data("suggested_PTF", envir = environment())
@@ -75,8 +75,8 @@ which_PTF <- function(predictor, target = NULL) {
         # query[query.test,]
         # DO MATHC ----
 
-        t1 <- lapply(apply(query,1, function(y){y[!is.na(y)]%in%c("USSAND", "USSILT","USCLAY","BD","DEPTH_M",  "CACO3", "PH_H2O")}), function(x) length(x)==length(c("USSAND", "USSILT","USCLAY","DEPTH_M", "BD", "CACO3", "PH_H2O")))
-        t2 <- lapply(apply(query,1, function(y){y[!is.na(y)]%in%c("USSAND", "USSILT","USCLAY","BD","DEPTH_M",  "CACO3", "PH_H2O")}), all)
+        t1 <- lapply(apply(query,1, function(y){y[!is.na(y)]%in%predictor}), function(x) length(x)==length(predictor))
+        t2 <- lapply(apply(query,1, function(y){y[!is.na(y)]%in%predictor}), all)
 
         result <- suggested_PTF[as.logical(unlist(t1)*unlist(t2)), target, with = FALSE]
 
